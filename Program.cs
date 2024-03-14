8436a258c833a07809957f0452b393b9df627eb2
yomma174 <61536753+yomma174@users.noreply.github.com>
2024-03-14 11:55:41 +0500

    first commit second-branch-praktika-3.4

--- Program.cs
+++ Program.cs
@@ -10,6 +10,8 @@ namespace new_project
     {
         static void Main(string[] args)
         {
+            // second-branch-praktika-3.4
+            
             Console.WriteLine("Hello, world!");
 
             Console.ReadKey();
